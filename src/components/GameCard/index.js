9dbe8784d86056ccdd6a17d0fa371e6ab684dac8
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-08-24 00:32:02 -0500

    Updated image cards into class

--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import "./style.css";
 
-function GameCard(props) {
-    return (
-        <div class="game-image" id={props.id}>
-            <img alt={props.name} src={props.image} id={props.id} className="" />
-        </div>
-    )
+let score = 0;
+
+class GameCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isClicked: false };
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        e.preventDefault();        
+        console.log("The image was clicked.", this.props.id);
+        this.setState(state => ({
+            isClicked: true
+        }));
+        console.log(this.state.isClicked);
+    }
+
+    render() {
+        return (
+            <>
+                <div className="game-image hover14" id={this.props.id}>
+                    <figure><img alt={this.props.name} src={this.props.image} id={this.props.id} className="" onClick={this.handleClick} /></figure>
+                </div>
+            </>
+        );
+    }
 }
+
 
 export default GameCard;
