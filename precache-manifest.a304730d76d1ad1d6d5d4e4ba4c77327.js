275fae81696ac2a54bd70c5a184980ca8dd0b756
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-08-26 14:26:31 -0500

    Updates

---
+++ precache-manifest.a304730d76d1ad1d6d5d4e4ba4c77327.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f01ead4763afee860be731511d5a012",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "4af2bb3a23a23cbe4a3d",
+    "url": "/clickygame/static/css/main.041dc41d.chunk.css"
+  },
+  {
+    "revision": "fddebd62c6ce682d42e5",
+    "url": "/clickygame/static/js/2.cd371cab.chunk.js"
+  },
+  {
+    "revision": "4af2bb3a23a23cbe4a3d",
+    "url": "/clickygame/static/js/main.dfed2ed0.chunk.js"
+  },
+  {
+    "revision": "97ab0a19df7bee5dc929",
+    "url": "/clickygame/static/js/runtime~main.473dc39e.js"
+  }
+]);
